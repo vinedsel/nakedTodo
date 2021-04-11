3ee6af6d663dcdd389936483f4fd30bf1ab7dcb2
Raymond Gregory <vinedsel@gmail.com>
2021-04-10 23:17:28 -0500

    Built list function

--- todos.js
+++ todos.js
@@ -1,6 +1,17 @@
 let input = prompt("What would you like to do?");
 
+const todos = ['Add a new task'];
+
 while(input !== 'quit' && input !== 'q') {
+    if(input === 'list' || input === 'l') {
+        console.log('*************');
+        for(i=0; i<todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log('*************');
+    };
     input = prompt("What would you like to do?");
 }
 console.log("Quit successful");
+
+ 
