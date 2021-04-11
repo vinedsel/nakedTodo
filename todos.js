344b07b291e4c487d48fb79f36eb45baf820a93f
Raymond Gregory <vinedsel@gmail.com>
2021-04-11 00:06:47 -0500

    Built delete function

--- todos.js
+++ todos.js
@@ -14,6 +14,10 @@ while(input !== 'quit' && input !== 'q') {
 	const newTodo = prompt('Enter new task');
 	todos.push(newTodo);
   console.log(`${newTodo} added`);
+} else if(input === 'delete' || input === 'd') {
+  const index = prompt('Delete by index[#]');
+  const deleted = todos.splice(index, 1);
+  console.log(`Deleted ${deleted[0]}`);
 }
 	input = prompt("What would you like to do?");
 }
