5e91ab87aa83569b1500d4ea3b2d3e26502c13e4
Raymond Gregory <vinedsel@gmail.com>
2021-04-10 23:51:08 -0500

    Made small mod to newTodo

--- todos.js
+++ todos.js
@@ -1,17 +1,21 @@
-let input = prompt("What would you like to do?");
+/* eslint-disable no-console */
+let input = prompt('What would you like to do?');
 
 const todos = ['Add a new task'];
 
 while(input !== 'quit' && input !== 'q') {
-    if(input === 'list' || input === 'l') {
-        console.log('*************');
-        for(i=0; i<todos.length; i++) {
-            console.log(`${i}: ${todos[i]}`);
-        }
-        console.log('*************');
-    };
-    input = prompt("What would you like to do?");
+	if(input === 'list' || input === 'l') {
+		console.log('*************');
+		for(i=0; i<todos.length; i++) {
+			console.log(`${i}: ${todos[i]}`);
+}
+		console.log('*************');
+} else if(input === 'new' || input === 'n') {
+	const newTodo = prompt('Enter new task');
+	todos.push(newTodo);
+  console.log(`${newTodo} added`);
+}
+	input = prompt("What would you like to do?");
 }
 console.log("Quit successful");
 
- 
